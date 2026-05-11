323b144bc17db93088d8d131a6943162c3aa1ab3
ErPyCode <erpycode@hotmail.com>
2026-05-11 13:00:48 +0330

    add "copy to clipboard" option by user choice

--- Password_genartor.py
+++ Password_genartor.py
@@ -1,8 +1,16 @@
-import random
 import secrets
+import pyperclip
 
 
-def Pass_word_picker(user_choice):
+def Pass_Word_Picker(user_choice):
+    """this func generate password
+
+    Args:
+        user_choice (int): user choose its lenth of pass
+
+    Returns:
+        _type_: generated password
+    """
     count = 1
     Gen_Password = ""
     pass_list = [
@@ -21,6 +29,11 @@ def Pass_word_picker(user_choice):
 
 
 def PassWord_Saver(genpassword):
+    """this func save gen password to txt file
+
+    Args:
+        genpassword (string): password that Genrated
+    """
     with open("Password.txt", "w", encoding="utf-8") as file:
         file.write(genpassword)
 
@@ -31,7 +44,10 @@ if user < 8 or user > 20:
         user = int(input("Please Enter Number Between 8 To 20 :  "))
 Genarated_Password = Pass_word_picker(user)
 
-choice = input("For Saving Your Password Enter y : ")
+choice = input("For Saving Your Password Enter y (for copy in your clipbord press c): ")
 
 if choice == "y" or choice == "Y":
     PassWord_Saver(Genarated_Password)
+elif choice == "c" or choice == "C":
+    pyperclip.copy(Genarated_Password)
+    print("Coppied to Clipboard!")
